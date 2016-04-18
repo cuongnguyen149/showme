0e62845038c816bc7b4ee0d24aa098e1c592fc4b
Cuong Nguyen <cuongnguyen149@gmail.com>
2016-04-19 00:18:25 +0700

    support form data

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -72,43 +72,44 @@ function registerUser(req, res) {
 * User login API (Integrate with QuickBlox)
 */
 function login(req, res){
-  var userObject = req.swagger.params.login.value;
-  var params = {email : userObject.email, password: userObject.pwd};
-  quickbloxConfig.createSession(function(err, result) {
-    if(result){
-      quickbloxConfig.login(params, function(err, result) {
-        if(result){
-          // console.log("login success!!!!");
-          var query = "SELECT *, NULL AS " + constants.PWD + 
-                      " FROM " + constants.CLIENT_USER +
-                      " WHERE "  + constants.USER_ID + " = "  + result.id;
-          var update = "UPDATE " + constants.CLIENT_USER +
-                       " SET " + constants.DEVICE_UIID + " = '" + userObject.device_uiid + "'" +
-                       " WHERE " + constants.USER_ID + " = "  + result.id;
-          dbConfig.query(update, function(err, rows){
-            if(rows){
-              dbConfig.query(query, function(err, rows){
-               if(err){
-                console.log(err);
-                res.json(myUtils.createDatabaseError(err)); 
-               }else{
-                // console.log(myUtils.generateToken(rows[0]));
-                res.json({returnCode : constants.SUCCESS_CODE, message: "Login successfully", data: myUtils.generateToken(rows[0])});  
-               } 
-              });
-            }else{
-              console.log(err);
-              res.json(myUtils.createDatabaseError(err));
-            }
-          });             
-        }else{
-          res.json(myUtils.createErrorStr('Login fail. Incorrect Email or Password. Please try again.', constants.ERROR_CODE));
-        }
-      });
-    }else{
-      res.json(myUtils.createQuickBloxError(err)); 
-    }
-  });    
+  console.log(req.swagger.params);
+  // var userObject = req.swagger.params.login.value;
+  // var params = {email : userObject.email, password: userObject.pwd};
+  // quickbloxConfig.createSession(function(err, result) {
+  //   if(result){
+  //     quickbloxConfig.login(params, function(err, result) {
+  //       if(result){
+  //         // console.log("login success!!!!");
+  //         var query = "SELECT *, NULL AS " + constants.PWD + 
+  //                     " FROM " + constants.CLIENT_USER +
+  //                     " WHERE "  + constants.USER_ID + " = "  + result.id;
+  //         var update = "UPDATE " + constants.CLIENT_USER +
+  //                      " SET " + constants.DEVICE_UIID + " = '" + userObject.device_uiid + "'" +
+  //                      " WHERE " + constants.USER_ID + " = "  + result.id;
+  //         dbConfig.query(update, function(err, rows){
+  //           if(rows){
+  //             dbConfig.query(query, function(err, rows){
+  //              if(err){
+  //               console.log(err);
+  //               res.json(myUtils.createDatabaseError(err)); 
+  //              }else{
+  //               // console.log(myUtils.generateToken(rows[0]));
+  //               res.json({returnCode : constants.SUCCESS_CODE, message: "Login successfully", data: myUtils.generateToken(rows[0])});  
+  //              } 
+  //             });
+  //           }else{
+  //             console.log(err);
+  //             res.json(myUtils.createDatabaseError(err));
+  //           }
+  //         });             
+  //       }else{
+  //         res.json(myUtils.createErrorStr('Login fail. Incorrect Email or Password. Please try again.', constants.ERROR_CODE));
+  //       }
+  //     });
+  //   }else{
+  //     res.json(myUtils.createQuickBloxError(err)); 
+  //   }
+  // });    
 };
 /**
 * User update user's role API.

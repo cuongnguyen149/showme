af6880ad6f3827262a9bcb858a809a804385a934
Cuong Nguyen <cuongnguyen149@gmail.com>
2016-04-19 19:17:26 +0700

    back json:

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -15,16 +15,11 @@ module.exports = {
 * Register API (Integrate with QuickBlox)
 */
 function registerUser(req, res) {
-  var email = req.swagger.params.email.value,
-      pwd = req.swagger.params.pwd.value,
-      firstname = req.swagger.params.firstname.value,
-      lastname = req.swagger.params.lastname.value,
-      dob = req.swagger.params.dob.originalValue,
-      device_uiid = req.swagger.params.device_uiid.value;        
-  var params = {email : email, password: pwd};
+  var userObject = req.swagger.params.user.value;
+  var params = {email : userObject.email, password: userObject.pwd};
   var query_email = "SELECT " + constants.EMAIL + 
                     " FROM " + constants.CLIENT_USER +
-                    " WHERE "  + constants.EMAIL + " = '"  + email + "'";
+                    " WHERE "  + constants.EMAIL + " = '"  + userObject.email + "'";
   dbConfig.query(query_email, function(err, rows){
     if(rows){
       if(rows.length > 0){
@@ -38,9 +33,10 @@ function registerUser(req, res) {
                 var insert = "INSERT INTO " + constants.CLIENT_USER +  
                             " (" + constants.USER_ID + ", " + constants.EMAIL + ", " + constants.PWD + ", " + constants.FIRSTNAME + ", " + constants.LASTNAME + ", " + constants.DOB + ", " + constants.DEVICE_UIID + ") " +
                             "VALUES " +
-                            "( "+ result.id + ", '"+ email + "', '"+ pwd + "', '" + firstname + "', '" + lastname + "', '" + dob + "', '" + device_uiid + "');";
+                            "( "+ result.id + ", '"+ userObject.email + "', '"+ userObject.pwd + "', '" + userObject.firstname + "', '" + userObject.lastname + "', '" + userObject.dob + "', '" + userObject.device_uiid + "');";
                 dbConfig.query(insert, function(err, rows){
                    if(err){
+                    // console.log(err);
                     res.json(myUtils.createDatabaseError(err)); 
                    }else{
                     var query = "SELECT *, NULL AS " + constants.PWD + 
@@ -48,6 +44,7 @@ function registerUser(req, res) {
                                 " WHERE "  + constants.USER_ID + " = "  + result.id;
                     dbConfig.query(query, function(err, rows){
                      if(err){
+                      // console.log(err);
                       res.json(myUtils.createDatabaseError(err)); 
                      }else{
                       // console.log(rows[0]);
@@ -75,10 +72,8 @@ function registerUser(req, res) {
 * User login API (Integrate with QuickBlox)
 */
 function login(req, res){
-  var email = req.swagger.params.email.value,
-      pwd = req.swagger.params.pwd.value,
-      device_uiid = req.swagger.params.device_uiid.value; 
-  var params = {email : email, password: pwd};
+  var userObject = req.swagger.params.login.value;
+  var params = {email : userObject.email, password: userObject.pwd};
   quickbloxConfig.createSession(function(err, result) {
     if(result){
       quickbloxConfig.login(params, function(err, result) {
@@ -88,12 +83,13 @@ function login(req, res){
                       " FROM " + constants.CLIENT_USER +
                       " WHERE "  + constants.USER_ID + " = "  + result.id;
           var update = "UPDATE " + constants.CLIENT_USER +
-                       " SET " + constants.DEVICE_UIID + " = '" + device_uiid + "'" +
+                       " SET " + constants.DEVICE_UIID + " = '" + userObject.device_uiid + "'" +
                        " WHERE " + constants.USER_ID + " = "  + result.id;
           dbConfig.query(update, function(err, rows){
             if(rows){
               dbConfig.query(query, function(err, rows){
                if(err){
+                console.log(err);
                 res.json(myUtils.createDatabaseError(err)); 
                }else{
                 // console.log(myUtils.generateToken(rows[0]));
@@ -101,6 +97,7 @@ function login(req, res){
                } 
               });
             }else{
+              console.log(err);
               res.json(myUtils.createDatabaseError(err));
             }
           });             
@@ -117,26 +114,25 @@ function login(req, res){
 * User update user's role API.
 */
 function updateRole (req, res){
-  var user_id = req.swagger.params.user_id.value,
-      role = req.swagger.params.role.value;
-  var role_update = "user";
-  if(role && role == "user"){
-    role_update = "leader";
-    query = "SELECT *, NULL AS " + constants.PWD + 
-              " FROM " + constants.CLIENT_USER +
-              " WHERE "  + constants.USER_ID + " = "  + user_id;
-  }
+  var userObject = req.swagger.params.user.value;
+  var role = "user";
   var query = "SELECT *, NULL AS " + constants.PWD + ", NULL AS " + constants.FEE_PER_HOUR + ", NULL AS " + constants.MONTH_INCOME +
               " FROM " + constants.CLIENT_USER +
-              " WHERE "  + constants.USER_ID + " = "  + user_id;
+              " WHERE "  + constants.USER_ID + " = "  + userObject.user_id;
   var update = "UPDATE " + constants.CLIENT_USER +
-               " SET " + constants.ROLE + " = '" + role_update  + "' " +
-               " WHERE " + constants.USER_ID + " = "  + user_id;             
+               " SET " + constants.ROLE + " = '" + role  + "' " +
+               " WHERE " + constants.USER_ID + " = "  + userObject.user_id;             
+  if(userObject.role && userObject.role == "user"){
+    role = "leader";
+    query = "SELECT *, NULL AS " + constants.PWD + 
+              " FROM " + constants.CLIENT_USER +
+              " WHERE "  + constants.USER_ID + " = "  + userObject.user_id;
+  }
   dbConfig.query(update, function(err, rows){
     if(rows){
       dbConfig.query(query, function(err, rows){
         if(rows){
-          res.json({returnCode: constants.SUCCESS_CODE, message: "Updated user " + user_id + " to " + role_update + " role.", data : {user :rows[0]}});
+          res.json({returnCode: constants.SUCCESS_CODE, message: "Updated user " + userObject.user_id + " to " + role + " role.", data : {user :rows[0]}});
         }else{
           res.json(myUtils.createDatabaseError(err));    
         }
@@ -159,7 +155,7 @@ function updateUserProfiles(req, res){
                     " WHERE "  + constants.EMAIL + " = '"  + email + "'";
   dbConfig.query(query_email, function(err, rows){
     if(rows){
-      if(rows.length > 0 && rows[0].email != userObject.email){
+      if(rows.length > 0 && row[0].email != userObject.email){
         res.json(myUtils.createErrorStr('Email already exist.', constants.ERROR_CODE));
       }else{
         quickbloxConfig.createSession(function(err, result) {

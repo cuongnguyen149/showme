8bc92d1b5279e4612bce2b7467e687995b52fce5
Cuong Nguyen <cuongnguyen149@gmail.com>
2016-04-26 23:58:17 +0700

    update status socket/leader.js

--- api/controllers/socket/leader.js
+++ api/controllers/socket/leader.js
@@ -10,8 +10,14 @@ var geocoder 			= require('node-geocoder')(geocoderProvider, httpAdapter);
 exports = module.exports = function(io){
 	var leader = {}; 
   	io.sockets.on('connection', function (socket) {
-	    console.log(socket.server.eio.clientsCount);
-	    socket.on('updateLocation', function (leaderObject) {
+  		console.log("connected");
+  		// console.log(socket);
+	    // console.log(socket.server.eio.clientsCount);
+	    /*
+	    * update leader location 
+	    * Param: user_id, address.	
+	    */
+	    socket.on('updateLeaderLocation', function (leaderObject) {
 	    	console.log(leaderObject);
 	    	if(leaderObject.user_id && leaderObject.address){
 		      	geocoder.geocode(leaderObject.address)
@@ -58,10 +64,45 @@ exports = module.exports = function(io){
 	    	// console.log(leaderObject);
 	    	// console.log(leader[socket.id]);
 	    });
-	    
+	    /*
+	    * update leader location 
+	    * Param: user_id, active.	
+	    */
+	    socket.on("updateLeaderStatus", function(leaderObject){
+	    	leader[socket.id] =  leaderObject.user_id;
+	    	var update = "UPDATE " + constants.CLIENT_USER +
+	          	 " SET " + constants.IS_ACTIVE + " = " + !leaderObject.active +
+	          	 " WHERE " + constants.USER_ID + " = ?";
+			var query = "SELECT *, NULL AS " + constants.PWD + ", DATE_FORMAT( " + constants.DOB + ", '%Y-%m-%d') AS "+  constants.DOB +
+		                          " FROM " + constants.CLIENT_USER +
+		                          " WHERE "  + constants.USER_ID + " = ?";          	 
+			dbConfig.query(update, [leaderObject.user_id], function(err, rows){
+				if(rows && rows.affectedRows > 0){
+					dbConfig.query(query, [leaderObject.user_id], function(err, rows){
+						if(rows && rows.length > 0){
+							socket.emit("updateStatusSuccess", {returnCode: constants.SUCCESS_CODE, message: "Updated status of leader " + leaderObject.user_id + " to " + !leaderObject.active +".", data: {user: rows[0]}});	
+							if(leaderObject.active){
+								// console.log(io.sockets.connected[socket.id]);
+								io.sockets.connected[socket.id].disconnect();
+								console.log(socket.server.eio.clientsCount);
+							}
+							console.log(socket.server.eio.clientsCount);
+						}else{
+							socket.emit("updateStatusFalse", myUtils.createDatabaseError(err));
+						}
+					});
+				}else if(err){
+					socket.emit("updateStatusFalse", myUtils.createDatabaseError(err));
+				}else{
+					socket.emit("updateStatusFalse", myUtils.createErrorStr("user_id: " +  leaderObject.user_id + " does not exist!", constants.ERROR_CODE));
+				}
+			});
+	    });
+
 	    socket.on('disconnect', function(leaderObject){
-	    	// console.log('user disconnected');
+	    	console.log('user disconnected');
 	    	// console.log(leader[socket.id]);
+
 	  	});
  	});
 }

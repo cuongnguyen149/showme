425521c39bd0867a01e354476722e846cb82f5bf
cuongnguyen <cuongnguyen149@gmail.com>
2016-06-09 21:15:19 +0700

    if amount = 0 => dont checkout

--- api/controllers/payment.js
+++ api/controllers/payment.js
@@ -34,34 +34,37 @@ function createTransaction (req, res){
 			res.json(myUtils.createDatabaseError(err));
 		}else if(rows && rows.length > 0){
 			var amount = rows[0].total + tip;
-			braintreeConfig.transaction.sale({
-			  customerId: rows[0].user_id,
-			  amount: amount
-			}, function (err, result) {
-				if(err){
-					res.json(myUtils.createErrorStr("Have problem with payment system! Please try it later. " + err, constants.ERROR_CODE));
-				}else if(result.success){
-					console.log(result);
-					transactionObject.currency = result.transaction.currencyIsoCode;
-					transactionObject.amount   = result.transaction.amount;
-					transactionObject.user 	   = rows[0].user_id;
-					if(tip > 0){
-						console.log(update_transaction);
-						dbConfig.query(update_transaction, [tip, amount, transactionObject.id], function (err, rows) {
-							if(err){
-								res.json(myUtils.createErrorStr("Have problem with payment system! Please try it later. " + err, constants.ERROR_CODE));
-							}else{
-								res.json({returnCode: constants.SUCCESS_CODE, message : "Checkout success!.", data: {transaction: transactionObject}});
-							}
-						})
+			if(amount != 0){
+				braintreeConfig.transaction.sale({
+				  customerId: rows[0].user_id,
+				  amount: amount
+				}, function (err, result) {
+					if(err){
+						res.json(myUtils.createErrorStr("Have problem with payment system! Please try it later. " + err, constants.ERROR_CODE));
+					}else if(result.success){
+						console.log(result);
+						transactionObject.currency = result.transaction.currencyIsoCode;
+						transactionObject.amount   = result.transaction.amount;
+						transactionObject.user 	   = rows[0].user_id;
+						if(tip > 0){
+							console.log(update_transaction);
+							dbConfig.query(update_transaction, [tip, amount, transactionObject.id], function (err, rows) {
+								if(err){
+									res.json(myUtils.createErrorStr("Have problem with payment system! Please try it later. " + err, constants.ERROR_CODE));
+								}else{
+									res.json({returnCode: constants.SUCCESS_CODE, message : "Checkout success!.", data: {transaction: transactionObject}});
+								}
+							})
+						}else{
+							res.json({returnCode: constants.SUCCESS_CODE, message : "Checkout success!.", data: {transaction: transactionObject}});
+						}
 					}else{
-						res.json({returnCode: constants.SUCCESS_CODE, message : "Checkout success!.", data: {transaction: transactionObject}});
+						res.json(myUtils.createErrorStr("Have problem with payment system! Please try it later. " + result.message, constants.ERROR_CODE));
 					}
-				}else{
-					res.json(myUtils.createErrorStr("Have problem with payment system! Please try it later. " + result.message, constants.ERROR_CODE));
-				}
-			});
-			
+				});	
+			}else{
+				res.json(myUtils.createErrorStr("Cannot checkout when amount = 0", constants.ERROR_CODE));
+			}
 		}else{
 			res.json(myUtils.createErrorStr("Id does not exist. Please check again.", constants.ERROR_CODE));
 		}
